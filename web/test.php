3873cc1cbf5c50c6853fac21771d161b935383fb
21021785 <21021785@myrp.edu.sg>
2023-01-03 23:08:00 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -1,5 +1,9 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+  <title> Home </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Hello World!"> 
 </head>
 <body>
   Hello World!
